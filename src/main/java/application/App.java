3e35884e1bb3f9b4fa502acf57677ecb36054405
Francisco Javier Martinez Lago <edoreld@gmail.com>
2019-02-24 09:59:15 +0100

    src

---
+++ src/main/java/application/App.java
@@ -0,0 +1,9 @@
+package application;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class App extends Application {
+	/** Empty. The server will then discover all resource classes automatically. */
+}
